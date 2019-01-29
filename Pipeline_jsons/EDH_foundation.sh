3dfc22342eb4326c22fd753a5098afab398d8aff
priyata1306 <46705226+priyata1306@users.noreply.github.com>
2019-01-29 15:08:16 +0530

    Add files via upload

---
+++ Pipeline_jsons/EDH_foundation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Please find the updated files"
+echo "Please find the updated files"
+sed -i 's/development/'$1'/g' fndcmpnt.parameterupdated.json
+sed -i 's/development/'$1'/g' rg_creation.ps1
