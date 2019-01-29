67d6e7ed04751d37b53a6942fb52b77706c8e7dd
priyata1306 <46705226+priyata1306@users.noreply.github.com>
2019-01-29 16:00:34 +0530

    Update EDH_foundation.sh

--- Pipeline_jsons/EDH_foundation.sh
+++ Pipeline_jsons/EDH_foundation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Please find the updated files"
 echo "Please find the updated files"
-sed -i 's/development/'$1'/g' fndcmpnt.paramter.json
+sed -i 's/development/'$1'/g' fndcmpnt.paramter11.json
 sed -i 's/development/'$1'/g' rg_creation.ps1
